d3f2ca664d91b9a5e6ae065839299e87fb9073d9
LeBaptouBaptiste <baptiste.vidal.2005@gmail.com>
2024-12-17 14:08:25 +0100

    Ajout de la gestion des differentes routes

--- assets/app.js
+++ assets/app.js
@@ -3,5 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './styles/app.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+// Vérifie si l'élément avec id 'root' existe
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(<App page={"home"} />);
+}
+
+// Vérifie si l'élément avec id 'classement' existe
+const classementElement = document.getElementById('classement');
+if (classementElement) {
+    const classement = ReactDOM.createRoot(classementElement);
+    classement.render(<App page={"Classement"} />);
+}
--- assets/components/App.jsx
+++ assets/components/App.jsx
@@ -6,13 +6,30 @@ const classesData = [
   { nom: "Classe B", etudiants: 25, tours: 110 },
   { nom: "Classe C", etudiants: 28, tours: 95 },
   { nom: "Classe D", etudiants: 32, tours: 130 },
+  { nom: "Classe E", etudiants: 27, tours: 115 },
+  { nom: "Classe F", etudiants: 29, tours: 105 },
+  { nom: "Classe G", etudiants: 33, tours: 140 },
+  { nom: "Classe H", etudiants: 24, tours: 100 },
+  { nom: "Classe I", etudiants: 26, tours: 125 },
+  { nom: "Classe J", etudiants: 31, tours: 135 },
+  { nom: "Classe K", etudiants: 22, tours: 90 },
+  { nom: "Classe L", etudiants: 35, tours: 145 },
+  { nom: "Classe M", etudiants: 30, tours: 110 },
+  { nom: "Classe N", etudiants: 28, tours: 102 },
+  { nom: "Classe O", etudiants: 20, tours: 85 },
 ];
 
-function App() {
+function App( {page} ) {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 flex items-center justify-center">
-      <Classement data={classesData} />
-    </div>
+    <>
+      <div className="bg-gradient-to-br from-gray-900 via-gray-750 to-gray-900">
+        <div className="px-16 py-16">
+          {page == "Classement" && 
+            <Classement data={classesData} />
+          }
+        </div>
+      </div>
+    </>
   );
 }
 
