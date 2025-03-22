6a22387cb24546575c81f362f4ec3a727198d14a
Maxime Baude <contact.mbaude@gmail.com>
2025-03-22 15:10:10 +0100

    Gestion de l'hydratation !

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { socket } from "../utils/socket";
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   const [isConnected, setIsConnected] = useState(socket.connected);
@@ -30,10 +31,11 @@ export default function RootLayout({ children }) {
       <body className="h-full">
         <main>{children}</main>
         {/* Confettis pour la page Podium */}
-        <script
+        <Script
           data-trigger="custom"
           src="https://run.confettipage.com/here.js"
-          data-confetticode="U2FsdGVkX1+ZiPDGcXsNuSCOWFwRY5wW5vx7N+h5xl7t8ALXVSs/hOMypj+U7iDJKMIFkmBN8X+lf1fWzWKANISBtCz23Kf/y+iCii4KQIh7UCwLnmb2RQqhwzPoNnYAbstRww90AqKqldFqPE8/80WTxNaZCIebs4Xa9opFeS3deZMYJQFKt7dN5vWl1HC9Hx2T+CLxrly+78DJFxHQ31lckBt/oIr8kaL42WxQqQlV5DNyPVHxFd0JEtKGF2UcmpgaH5+A5hrp5fNuT7th3wM95ASGkYmlN8eBFyI6ivfigb93C0DykMba81M8P8b+nfpo6cj4T8sgdPDoqDFM71iwR2apxhgpKGobrHok6PP77MWuCCkWYcUvKVWjjmEWP9NkYm31mdih/vzHxX9H6Qkt/feaZCl0BQvmF5xVe18j0jupEgN99/7aW48iaxUadJCNIjlMn57TJyFF0PXyp/MD7FEJuGLVK9brZ9Kj1ZNU7/fabJr0qU5G4GFrD0eABMqgSrnnByJnU+IgeKzidGVd8XHVTSJldOXZskI6BVsXI9DYvSJBUkCZAho6NefRJyvAKmgmalDhmhHJJ4LB5+btqGziYLBrPqlPEZCF4rZ13A7X2tETKsdz2ZcC1efaMGvrmTQZmqF6US8Jqkf84wb4kDoKMYsSGrBxUpbLWHo/TjSR5RFI4uKGMj6Cm6NDC5NRtcAtMffpr+KCzoE5hmqRi9IQzS+peRo0xsTPZ1NNOqZ3OIilaUvQGY0MgtLf"></script>
+          data-confetticode="U2FsdGVkX1+ZiPDGcXsNuSCOWFwRY5wW5vx7N+h5xl7t8ALXVSs/hOMypj+U7iDJKMIFkmBN8X+lf1fWzWKANISBtCz23Kf/y+iCii4KQIh7UCwLnmb2RQqhwzPoNnYAbstRww90AqKqldFqPE8/80WTxNaZCIebs4Xa9opFeS3deZMYJQFKt7dN5vWl1HC9Hx2T+CLxrly+78DJFxHQ31lckBt/oIr8kaL42WxQqQlV5DNyPVHxFd0JEtKGF2UcmpgaH5+A5hrp5fNuT7th3wM95ASGkYmlN8eBFyI6ivfigb93C0DykMba81M8P8b+nfpo6cj4T8sgdPDoqDFM71iwR2apxhgpKGobrHok6PP77MWuCCkWYcUvKVWjjmEWP9NkYm31mdih/vzHxX9H6Qkt/feaZCl0BQvmF5xVe18j0jupEgN99/7aW48iaxUadJCNIjlMn57TJyFF0PXyp/MD7FEJuGLVK9brZ9Kj1ZNU7/fabJr0qU5G4GFrD0eABMqgSrnnByJnU+IgeKzidGVd8XHVTSJldOXZskI6BVsXI9DYvSJBUkCZAho6NefRJyvAKmgmalDhmhHJJ4LB5+btqGziYLBrPqlPEZCF4rZ13A7X2tETKsdz2ZcC1efaMGvrmTQZmqF6US8Jqkf84wb4kDoKMYsSGrBxUpbLWHo/TjSR5RFI4uKGMj6Cm6NDC5NRtcAtMffpr+KCzoE5hmqRi9IQzS+peRo0xsTPZ1NNOqZ3OIilaUvQGY0MgtLf"
+          strategy="afterInteractive"></Script>
       </body>
     </html>
   );
