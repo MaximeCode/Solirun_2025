5f159ba648cd3d36c22d4fb44e67565fc9c52008
LeBaptouBaptiste <153990224+LeBaptouBaptiste@users.noreply.github.com>
2025-05-12 17:16:01 +0200

    Create config.php

---
+++ API/config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('SECURE_ACCESS') or die('Accès direct au fichier non autorisé');
+
+return [
+  'DB_HOST' => 'ip hôte',
+  'DB_PORT' => 'port',
+  'DB_NAME' => 'nom DB',
+  'DB_USER' => 'User DB',
+  'DB_PASSWORD' => 'Password User'
+];
